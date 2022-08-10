76b5246e8e89b62062c98ba32bc986e707b3b55a
Kássia <kassia_guterres@outlook.com>
2022-08-10 14:29:53 -0300

    Folder and Files organizarion

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "../style/globalStyles";
+import PrivatePage from "./privatePage/PrivatePage";
+import UserContext from "../context/UserContext";
+import SignUp from "./signUp/SignUp"
+import Subscritions from "./subscritions/Subscritions";
+import Home from "./home/Home";
+// import { useState } from "react";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+
+      <BrowserRouter>
+        <UserContext.Provider value={{}}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route
+              path="/subscritions"
+              element={
+                <PrivatePage>
+                  <Subscritions />
+                </PrivatePage>
+              }
+            />
+            <Route
+              path="/home"
+              element={
+                <PrivatePage>
+                  <Home />
+                </PrivatePage>
+              }
+            />
+          </Routes>
+        </UserContext.Provider>
+      </BrowserRouter>
+    </>
+  );
+}
---
+++ src/services/drivenPuls.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+const BASE_URL = "https://mock-api.driven.com.br/api/v4/driven-plus/";
+
+function postSignUp(body) {
+  const promise = axios.post(`${BASE_URL}/auth/sign-up`, body);
+  return promise;
+}
+
+function postLogin(body) {
+  const promise = axios.post(`${BASE_URL}/auth/login`, body);
+  return promise;
+}
+
+function getListPlans() {
+  const config = createHeaders();
+  const promise = axios.get(`${BASE_URL}/subscriptions/memberships`, config);
+  return promise;
+}
+
+function getListPlan(planId) {
+  const config = createHeaders();
+  const promise = axios.get(
+    `${BASE_URL}/subscriptions/memberships/${planId}`,
+    config
+  );
+  return promise;
+}
+
+function postSubscriptions() {
+  const config = createHeaders();
+  const promise = axios.post(`${BASE_URL}/subscriptions`, config);
+  return promise;
+}
+
+function deletePlan() {
+  const config = createHeaders();
+  const promise = axios.delete(`${BASE_URL}/subscriptions`, config);
+  return promise;
+}
+
+function createHeaders() {
+  const auth = JSON.parse(localStorage.getItem("drivenPlus"));
+  const config = {
+    headers: {
+      Authorization: `Bearer ${auth.token}`,
+    },
+  };
+
+  return config;
+}
+
+export {
+  postSignUp,
+  postLogin,
+  getListPlans,
+  getListPlan,
+  postSubscriptions,
+  deletePlan,
+  createHeaders,
+};
